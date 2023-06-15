33e4da465719a53acc28b45f3a4d3c5b29708b79
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2023-06-15 11:17:24 +0100

    start my portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import About from './components/About';
+import Project from './components/Project';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Testimonial from './components/Testimonial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <NavBar />
+      <About />
+      <Project />
+      <Skills />
+      <Testimonial />
+      <Contact />
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+      title: "Open Hospital Web",
+      subtitle: "React and Spring boot",
+      description:
+        "Open Hospital is a free and open source software developed and supported by Informatici Senza Frontiere to provide a sustainable tool for the management of hospitals, health centers and facilities.",
+      image: "./project-1.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "U2gesco",
+      subtitle: "Angular + Spring boot",
+      description:
+        "U2gesco is a secure, web-based registration management application dedicated to schools, enabling you to avoid paper printouts and manage registration requests chronologically.",
+      image: "./project-2.jpg",
+      link: "https://reedbarger.com",
+    },
+  ];
+
+export const skills = ["git", "github", "java", "Spring Boot", "Maven", "Tomcat", "Jasper", "PHP", "Spring MVC", "Spring Data JPA" ]
